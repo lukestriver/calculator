d129303f586e9ffc5effb1b7c2a29a1f746d2583
lukestriver <lukasz_rodzik@o2.pl>
2017-10-29 14:28:02 +0100

    Calculator

--- src/main/java/com/kodilla/kodillacourse/Calculator.java
+++ src/main/java/com/kodilla/kodillacourse/Calculator.java
@@ -2,6 +2,20 @@ package com.kodilla.kodillacourse;
 
 public class Calculator {
 
+    public int addXAndY(int x, int y) {
+        return x + y;
+    }
 
+    public int subtractXAndY(int x, int y) {
+        return x - y;
+    }
 
+        public static void main(String args[]) {
+        Calculator calculator = new Calculator();
+        int sum = calculator.addXAndY(15, 10);
+        int diff = calculator.subtractXAndY(25, 10);
+
+        System.out.println("Sum: " + sum + "\n" + "Diff.: " + diff);
+
+    }
 }
